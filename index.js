b19c6bf4e49f154828ff2f99bd73c9491ac449f7
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-12-06 17:24:48 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// MARK: -------------------- REQUIRED OBJECTS
+
+/* Create the port that we're connecting to */
+const port = 8080;
+
+/* Get a connection to the mySql database */
+const mySqlConnection = require('./dbHelpers/mySqlWrapper');
+
+/* The bearerTokenDBHelper handles all of the database operations
+relating to saving and retrieving oAuth2 bearer tokens */
+const bearerTokensDBHelper = require('./dbHelpers/bearerTokensDBHelper')(mySqlConnection);
+
+/* The userDBHelper handles all of the database operations relating
+to users such as registering and retrieving them */
+const userDBHelper = require('./dbHelpers/userDBHelper')(mySqlConnection);
+
+/* This is a library used to help parse the body of the api requests. */
+const bodyParser = require('body-parser');
+
+// Require express
+const express = require('express');
+
+// Initialize the express app
+const expressApp = express();
+
+// MARK: -------------------- REQUIRED OBJECTS
+
+// MARK: -------------------- SET UP MIDDLEWARE
+
+/* Set the bodyParser to parse the urlencoded post data */
+expressApp.use(bodyParser.urlencoded({extended: true}));
+
+// MARK: -------------------- SET UP MIDDLEWARE
+
+// MARK: -------------------- INITIALIZE THE SERVER
+
+// init the server
+expressApp.listen(port, () => {
+    console.log('listening on port ${port}');
+});
+
+// MARK: -------------------- INITIALIZE THE SERVER
