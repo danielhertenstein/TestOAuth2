991d92f5880fb5cd19e56694ec94250655abf1e4
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-12-06 17:40:39 -0500

    Starts on the userDBHelper file

---
+++ authorization/authRoutesMethods.js
@@ -0,0 +1,13 @@
+/* The userDBHelper is an object which will handle all of the user related db
+    operations such as saving new users or retrieving existing ones. You can find
+    it in userDBHelper.js in this projects databaseHelpers folder.
+ */
+let userDBHelper;
+
+/**
+ *
+ * This module exports a function which registers users by using
+ * the specified injectedUserDBHelper.
+ *
+ * @param injectedUserDBHelper - this object handles the execution
+ */
